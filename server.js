6c01ba205abde665021ecab6334e61bf0ed49097
SalaniTinko <muhammadarslantoor@gmail.com>
2021-04-18 01:47:09 +0500

    fix server.js

--- server.js
+++ server.js
@@ -5,11 +5,11 @@ const path = require('path');
 const app = express();
 
 // Serve only the static files form the dist directory
-app.use(express.static('./dist/payment-processor-dashboard'));
+app.use(express.static('./dist/client-gateway'));
 
 app.get('/*', (req, res) =>
-    res.sendFile('index.html', {root: 'dist/payment-processor-dashboard/'}),
+    res.sendFile('index.html', {root: 'dist/client-gateway/'}),
 );
 
 // Start the app by listening on the default Heroku port
-app.listen(process.env.PORT || 8080);
+app.listen(process.env.PORT || 8000);
